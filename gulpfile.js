a48d284dba879bbd5d345e1fd489b5cb93abdc48
rdizzle <r.loeffel@hispeed.ch>
2016-04-09 13:33:20 +0200

    gulp and less first steps

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,97 @@
 var gulp = require('gulp'),
-    connect = require('gulp-connect');
+    del = require('del'),
+    connect = require('gulp-connect'),
+    less = require('gulp-less'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssmin = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
+    jsmin = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin');
 
-gulp.task('webserver', function () {
-    connect.server();
+var src = 'src/',
+    dist = 'dist/',
+    srcJs = src + 'js/',
+    srcJsFiles = srcJs + '*.js',
+    srcLess = src + 'less/',
+    srcLessFiles = srcLess + '*.less',
+    srcImg = src + 'img/',
+    srcImgFiles = srcImg + '*'
+    srcHtmlFiles = src + '*.html',
+    distJs = dist + 'js/',
+    distCss = dist + 'css/'
+    distImg = dist + 'img/'
+    rootFiles = [src + 'sitemap.xml', src + 'robots.txt', src + '.htaccess'];
+
+gulp.task('clean', function(){
+    return del(dist);
 });
 
-gulp.task('default', ['webserver']);
+gulp.task('server', function(){
+    return connect.server({
+        port: 3000,
+        livereload: true,
+        root: 'dist'
+    });
+});
+
+gulp.task('less', function(){
+    return gulp.src(srcLessFiles)
+        .pipe(less())
+        .pipe(autoprefixer({
+            browsers: ['last 2 version'],
+            cascade: false
+        }))
+        .pipe(gulp.dest(distCss))
+        .pipe(cssmin())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(distCss))
+        .pipe(connect.reload());
+});
+
+gulp.task('js', function(){
+    return gulp.src(srcJsFiles)
+        .pipe(gulp.dest(distJs))
+        .pipe(jsmin())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(distJs))
+        .pipe(connect.reload());
+});
+
+gulp.task('html', function(){
+    return gulp.src(srcHtmlFiles)
+        .pipe(gulp.dest(dist))
+        .pipe(connect.reload());
+});
+
+gulp.task('img', function(){
+    return gulp.src(srcImgFiles)
+        .pipe(imagemin({
+            optimizationLevel: 5,
+            progressive: true,
+            multipass: true
+        }))
+        .pipe(gulp.dest(distImg))
+        .pipe(connect.reload());
+});
+
+gulp.task('rootFiles', function(){
+    return gulp.src(rootFiles)
+        .pipe(gulp.dest(dist))
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', ['build', 'server'], function(){
+    gulp.watch(srcHtmlFiles, ['html']);
+    gulp.watch(srcImgFiles, ['img']);
+    gulp.watch(srcJsFiles, ['js']);
+    gulp.watch(srcLessFiles, ['less']);
+    gulp.watch(rootFiles, ['rootFiles']);
+});
+
+gulp.task('default', ['clean', 'build', 'server', 'watch']);
+
+gulp.task('build', ['js', 'less', 'html', 'img', 'rootFiles']);
---
+++ src/js/script.js
@@ -0,0 +1,118 @@
+$(document).ready(function () {
+    "use strict";
+
+    var navHeight = $("header").outerHeight(),
+        aboutPos = null,
+        whatPos = null,
+        projectsPos = null,
+        contactPos = null,
+        scrollDisabled = false;
+
+    getPositions();
+
+    $("[data-scroll-target]").click(function (event) {
+        event.preventDefault();
+        var scrollTarget = "#" + event.target.dataset.scrollTarget;
+        $("html, body").animate({
+            scrollTop: $(scrollTarget).offset().top - navHeight - 30
+        }, 500);
+    });
+
+    $(".mobile-menu").click(function (event) {
+        event.preventDefault();
+        if ($(document).width() > 767) {
+            return null;
+        }
+        createMobileMenu();
+    });
+
+    $("body").on("click", ".mobile-menu-close", function (event) {
+        event.preventDefault();
+        destroyMobileMenu();
+    });
+
+    $("body").on("click", ".mobile-menu-container [data-scroll-target]", function (event) {
+        event.preventDefault();
+        destroyMobileMenu();
+        var scrollTarget = "#" + event.target.dataset.scrollTarget;
+        $("html, body").animate({
+            scrollTop: $(scrollTarget).offset().top - navHeight - 30
+        }, 500);
+    });
+
+    $(window).on("scroll", function () {
+        var scrollPos = $(this).scrollTop();
+
+        if (scrollPos > 0 && !($("header").hasClass("white"))) {
+            $("header").addClass("white");
+        }
+        if (scrollPos === 0 && $("header").hasClass("white")) {
+            $("header").removeAttr("class");
+        }
+
+        if (scrollPos < aboutPos && !($("a[data-scroll-target='top']").hasClass("active"))) {
+            $("a.active").removeClass("active");
+            $("header a[data-scroll-target='top']").addClass("active");
+        }
+        if (scrollPos >= aboutPos && scrollPos < whatPos && !($("a[data-scroll-target='about-me']").hasClass("active"))) {
+            $("a.active").removeClass("active");
+            $("header a[data-scroll-target='about-me']").addClass("active");
+        }
+        if (scrollPos >= whatPos && scrollPos < projectsPos && !($("a[data-scroll-target='what-i-do']").hasClass("active"))) {
+            $("a.active").removeClass("active");
+            $("header a[data-scroll-target='what-i-do']").addClass("active");
+        }
+        if (scrollPos >= projectsPos && scrollPos < contactPos && !($("a[data-scroll-target='projects']").hasClass("active"))) {
+            $("a.active").removeClass("active");
+            $("header a[data-scroll-target='projects']").addClass("active");
+        }
+        if (scrollPos >= contactPos && !($("a[data-scroll-target='contact']").hasClass("active"))) {
+            $("a.active").removeClass("active");
+            $("header a[data-scroll-target='contact']").addClass("active");
+        }
+    });
+
+    $(window).on("resize", function () {
+        getPositions();
+    });
+
+    $(document).on("touchmove", function (event) {
+        if (scrollDisabled) {
+            event.preventDefault();
+        }
+    });
+
+    function getPositions() {
+        aboutPos = $("h1#about-me").offset().top - navHeight - 31;
+        whatPos = $("h1#what-i-do").offset().top - navHeight - 31;
+        projectsPos = $("h1#projects").offset().top - navHeight - 31;
+        contactPos = $("h1#contact").offset().top - navHeight - 31;
+    }
+
+    function createMobileMenu() {
+        $("body").append("<div class='mobile-menu-container'></div>");
+        $(".mobile-menu-container").append("<a href='#' class='mobile-menu-close'><i class='fa fa-times'></i></a>");
+        $(".mobile-menu-container").append("<div class='center-container'></div>");
+        $(".mobile-menu-container .center-container").append("<ul></ul>");
+        $(".mobile-menu-container .center-container ul").append(
+            "<li><a href='#top' data-scroll-target='top'>Home</a></li>" +
+            "<li><a href='#about-me' data-scroll-target='about-me'>About Me</a></li>" +
+            "<li><a href='#what-i-do' data-scroll-target='what-i-do'>What I Do</a></li>" +
+            "<li><a href='#projects' data-scroll-target='projects'>Projects</a></li>" +
+            "<li><a href='#contact' data-scroll-target='contact'>Contact</a></li>"
+        );
+        $(".mobile-menu-container").animate({
+            "opacity": 1
+        }, 350);
+        scrollDisabled = true;
+    }
+
+    function destroyMobileMenu() {
+        $(".mobile-menu-container").animate({
+            "opacity": 0
+        }, 350, function () {
+            $(".mobile-menu-container").remove();
+        });
+        scrollDisabled = false;
+    }
+});
