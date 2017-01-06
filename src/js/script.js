3f63937488a7cfbf8985d68cda0ac6866429d0d5
Robin Löffel <r.loeffel@hispeed.ch>
2017-01-06 12:34:51 +0100

    :rainbow: navigation scroll reveal/hide

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const SCROLLERS = document.querySelectorAll('[data-scroll-link]'),
-    LOADERS = document.querySelectorAll('.load-reveal');
+    LOADERS = document.querySelectorAll('.load-reveal'),
+    NAV = document.querySelector('nav');
+
+let oldPos = window.pageYOffset;
 
 for (let i = 0; SCROLLERS.length > i; i++) {
     SCROLLERS[i].addEventListener('click', scrollToElement, false);
@@ -17,8 +20,36 @@ function scrollToElement(event) {
     });
 }
 
-window.onload = () => {
+function hideNav() {
+    NAV.classList.add('hidden');
+}
+
+function showNav() {
+    NAV.removeAttribute('class');
+}
+
+window.addEventListener('load', () => {
     for (let i = 0; LOADERS.length > i; i++) {
-        LOADERS[i].removeAttribute('class');
+        if (LOADERS[i].tagName == 'MAIN') {
+            LOADERS[i].removeAttribute('class');
+        } else {
+            LOADERS[i].classList.remove('load-reveal');
+        }
     }
-};
+}, false);
+
+window.addEventListener('scroll', () => {
+    let newPos = window.pageYOffset;
+
+    if (newPos > oldPos) {
+        if (!NAV.classList.contains('hidden')) {
+            hideNav();
+        }
+    } else {
+        if (NAV.classList.contains('hidden')) {
+            showNav();
+        }
+    }
+
+    oldPos = newPos;
+}, false);
